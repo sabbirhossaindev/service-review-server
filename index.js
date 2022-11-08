3ef7f6072226c0273bdd70ff4d4e7cf83f628257
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-08 16:50:33 +0600

    index file added

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const cors = require('cors');
+// const jwt = require('jsonwebtoken');
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// meddle Ware
+app.use(cors());
+app.use(express.json());
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ok9cief.mongodb.net/?retryWrites=true&w=majority`;
+
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// verifyJWT................
+// function verifyJWT(req, res, next) {
+//     const authHeader = (req.headers.authorization);
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'unauthorized access' });
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//         if (err) {
+//             return res.status(403).send({ message: 'Forbidden access' });
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+// }
+
+// async function run() {
+//     try {
+//         const serviceCollection = client.db('geniusCar').collection('services');
+//         const orderCollection = client.db('geniusCar').collection('orders');
+
+//         app.post('/jwt', (req, res) => {
+//             const user = req.body;
+//             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
+//             res.send({token})
+//         })
+
+//         // all router paba...
+//         app.get('/services', async (req, res) => {
+//             const query = {}
+//             const cursor = serviceCollection.find(query);
+//             const services = await cursor.toArray();
+//             res.send(services);
+//         });
+
+//         // specific 1 id for...
+//         app.get('/services/:id', async (req, res) => {
+//             const id = req.params.id;
+//             const query = { _id: ObjectId(id) };
+//             const service = await serviceCollection.findOne(query);
+//             res.send(service);
+//         });
+
+//         // orders api & added verifyJWT
+//         app.get('/orders', verifyJWT, async (req, res) => {
+            
+//             const decoded = req.decoded;
+            
+//             if(decoded.email !== req.query.email){
+//                 res.status(403).send({message: 'unauthorized access'})
+//             }
+
+//             let query = {};
+//             if (req.query.email) {
+//                 query = {
+//                     email: req.query.email
+//                 }
+//             }
+
+//             const cursor = orderCollection.find(query);
+//             const orders = await cursor.toArray();
+//             res.send(orders);
+//         });
+
+//         app.post('/orders', verifyJWT, async (req, res) => {
+//             const order = req.body;
+//             const result = await orderCollection.insertOne(order);
+//             res.send(result);
+//         });
+
+//         // update
+//         app.patch('/orders/:id', verifyJWT, async (req, res) => {
+//             const id = req.params.id;
+//             const status = req.body.status
+//             const query = { _id: ObjectId(id) }
+//             const updatedDoc = {
+//                 $set:{
+//                     status: status
+//                 }
+//             }
+//             const result = await orderCollection.updateOne(query, updatedDoc);
+//             res.send(result);
+//         })
+
+//         // delete
+//         app.delete('/orders/:id', verifyJWT, async (req, res) => {
+//             const id = req.params.id;
+//             const query = { _id: ObjectId(id) };
+//             const result = await orderCollection.deleteOne(query);
+//             res.send(result);
+//         })
+      
+//     }
+//     finally {
+      
+//     }
+//   }
+//   run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('doctor services is running')
+});
+
+app.listen(port, () => {
+    console.log(`doctor Servers Running on ${port}`);
+});
