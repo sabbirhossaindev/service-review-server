c67d2ac2929a1bdd21f217299119aff04257ac0b
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-09 16:04:35 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -15,108 +15,120 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ok9cief.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
-
-
+// console.log(uri);
 
 // verifyJWT................
-// function verifyJWT(req, res, next) {
-//     const authHeader = (req.headers.authorization);
-//     if (!authHeader) {
-//         return res.status(401).send({ message: 'unauthorized access' });
-//     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//         if (err) {
-//             return res.status(403).send({ message: 'Forbidden access' });
-//         }
-//         req.decoded = decoded;
-//         next();
-//     })
-// }
+function verifyJWT(req, res, next) {
+    const authHeader = (req.headers.authorization);
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
-// async function run() {
-//     try {
-//         const serviceCollection = client.db('geniusCar').collection('services');
-//         const orderCollection = client.db('geniusCar').collection('orders');
+async function run() {
+    try {
+        const serviceCollection = client.db('doctorServices').collection('services');
+        const orderCollection = client.db('doctorServices').collection('orders');
 
-//         app.post('/jwt', (req, res) => {
-//             const user = req.body;
-//             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
-//             res.send({token})
-//         })
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d'})
+            res.send({token})
+        })
 
-//         // all router paba...
-//         app.get('/services', async (req, res) => {
-//             const query = {}
-//             const cursor = serviceCollection.find(query);
-//             const services = await cursor.toArray();
-//             res.send(services);
-//         });
+        // all router paba...
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
 
-//         // specific 1 id for...
-//         app.get('/services/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: ObjectId(id) };
-//             const service = await serviceCollection.findOne(query);
-//             res.send(service);
-//         });
+        app.get('/limitsServices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        // app.get('/services-limit', async (req, res) => {
+        //     const query = {}
+        //     const myShort = {data: -1, time: -1}
+        //     const cursor = serviceCollection.find(query).short(myShort).limit(3);
+        //     const services = await cursor.toArray();
+        //     res.send(services);
+        // });
 
-//         // orders api & added verifyJWT
-//         app.get('/orders', verifyJWT, async (req, res) => {
+        // specific 1 id for...
+        // app.get('/services/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const service = await serviceCollection.findOne(query);
+        //     res.send(service);
+        // });
+
+        // orders api & added verifyJWT
+        // app.get('/orders', verifyJWT, async (req, res) => {
             
-//             const decoded = req.decoded;
+        //     const decoded = req.decoded;
             
-//             if(decoded.email !== req.query.email){
-//                 res.status(403).send({message: 'unauthorized access'})
-//             }
+        //     if(decoded.email !== req.query.email){
+        //         res.status(403).send({message: 'unauthorized access'})
+        //     }
 
-//             let query = {};
-//             if (req.query.email) {
-//                 query = {
-//                     email: req.query.email
-//                 }
-//             }
+        //     let query = {};
+        //     if (req.query.email) {
+        //         query = {
+        //             email: req.query.email
+        //         }
+        //     }
 
-//             const cursor = orderCollection.find(query);
-//             const orders = await cursor.toArray();
-//             res.send(orders);
-//         });
+        //     const cursor = orderCollection.find(query);
+        //     const orders = await cursor.toArray();
+        //     res.send(orders);
+        // });
 
-//         app.post('/orders', verifyJWT, async (req, res) => {
-//             const order = req.body;
-//             const result = await orderCollection.insertOne(order);
-//             res.send(result);
-//         });
+        // app.post('/orders', verifyJWT, async (req, res) => {
+        //     const order = req.body;
+        //     const result = await orderCollection.insertOne(order);
+        //     res.send(result);
+        // });
 
-//         // update
-//         app.patch('/orders/:id', verifyJWT, async (req, res) => {
-//             const id = req.params.id;
-//             const status = req.body.status
-//             const query = { _id: ObjectId(id) }
-//             const updatedDoc = {
-//                 $set:{
-//                     status: status
-//                 }
-//             }
-//             const result = await orderCollection.updateOne(query, updatedDoc);
-//             res.send(result);
-//         })
+        // // update
+        // app.patch('/orders/:id', verifyJWT, async (req, res) => {
+        //     const id = req.params.id;
+        //     const status = req.body.status
+        //     const query = { _id: ObjectId(id) }
+        //     const updatedDoc = {
+        //         $set:{
+        //             status: status
+        //         }
+        //     }
+        //     const result = await orderCollection.updateOne(query, updatedDoc);
+        //     res.send(result);
+        // })
 
-//         // delete
-//         app.delete('/orders/:id', verifyJWT, async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: ObjectId(id) };
-//             const result = await orderCollection.deleteOne(query);
-//             res.send(result);
-//         })
+        // delete
+        // app.delete('/orders/:id', verifyJWT, async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await orderCollection.deleteOne(query);
+        //     res.send(result);
+        // })
       
-//     }
-//     finally {
+    }
+    finally {
       
-//     }
-//   }
-//   run().catch(console.dir);
+    }
+  }
+  run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
